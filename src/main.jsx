c9c1d98b0870948ac1ddac6ca9067d62df1d0bf2
cbougiotas-eng <c.bougiotas@gmail.com>
2026-05-19 08:56:02 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TraderApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TraderApp />
+  </React.StrictMode>
+);
